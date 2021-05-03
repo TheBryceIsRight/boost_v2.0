6e04dbf89948086b0b7b536cf8ffdeeb47e23688
Bryce Watson <brycewatson315@gmail.com>
2021-05-02 20:50:11 -0400

    line chart flips to dark mode

--- migrations/001-helloworld.sql
+++ migrations/001-helloworld.sql
@@ -14,6 +14,13 @@ CREATE TABLE Vehicle (
     ownerId INTEGER REFERENCES Person(id)
 );
 
+INSERT INTO Person (firstName, lastName, email, password) values ('Wanda', 'Maximoff',  'theDarkHold@gmail.com', 'jbyM3&oXvcnmJFL2?~du7U=-&8eXAL*yr)nhshCo');
+INSERT INTO Person (firstName, lastName, email, password) values ('The', 'Vision', 'theMindStone@hotmail.com', 'yUf=kqjdmcgH@}oD!!H^cHybZD%],w#Tfw*?.h4}');
+
+-- INSERT INTO Vehicle (brand, model, ownerId) values('audi', 'R8', 1);
+-- INSERT INTO Vehicle (brand, model, ownerId) values('audi', 'R6', 1);
+-- INSERT INTO Vehicle (brand, model, ownerId) values('mercedes', 'benz', 2);
+
 -- Down
 DROP TABLE Person;
 DROP TABLE Vehicle;
--- src/pages/People.js
+++ src/pages/People.js
@@ -1,4 +1,6 @@
 import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
@@ -6,10 +8,36 @@ import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import fetch from 'isomorphic-unfetch';
+import LineChart from '../../components/LineChart';
+import LineChart2 from '../../components/LineChart2';
+import themeContext from '../../components/Theme';
+import { ThemeProvider } from '@material-ui/core/styles';
+
+
 
 export default function People({ list }) {
+
+
   return (
-    <TableContainer component={Paper}>
+    <ThemeProvider theme={themeContext}>
+    <Grid container spacing={6} direction='column'>
+      <Grid item>
+        <Typography variant='h3'>TCR/Discount</Typography>
+      </Grid>
+      <Grid item>
+      <LineChart/>
+        </Grid>
+        <Grid item>
+        <Typography variant='h3'>Total Discount Acceptance</Typography>
+      </Grid>
+        <Grid item>
+      <LineChart2/>
+        </Grid>
+        <Grid item>
+        <Typography variant='h3'>Current Users</Typography>
+       </Grid>
+        <Grid item>
+        <TableContainer component={Paper}>
       <Table aria-label="simple table">
         <TableHead>
           <TableRow>
@@ -33,6 +61,9 @@ export default function People({ list }) {
         </TableBody>
       </Table>
     </TableContainer>
+        </Grid>
+    </Grid>
+    </ThemeProvider>
   );
 }
 
